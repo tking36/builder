0caf95fba6f2f8cee44ab48d07adec4f95772087
Taylor King <taylorking@taylors-mbp.lan>
2023-04-14 17:39:15 -0400

    Home page, Service page, Contact page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,46 +1,51 @@
 import React, {useState} from 'react'
-import {Container, Nav, Navbar,Offcanvas, Image} from 'react-bootstrap'
+import {Container, Nav, Navbar,Offcanvas, Image, Button} from 'react-bootstrap'
 
 
 const Navigation = () => {
-  const [expand, updateExpanded] = useState(false)
-  
-
-  
 
   return (
     <>
-      
-        <Navbar key={expand} bg="light" expand={expand} className="mb-3 navigation">
-          <Container fluid>
-            <div>
-              <Image className='navbar-logo' src='https://i.imgur.com/UKU5YjC.jpg' ></Image>
-              <Navbar.Brand href="#">Builder King</Navbar.Brand>
-            </div>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                </Nav>
-                
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      
+    <Navbar className='navigation' >
+      <Container fluid>
+        <Image className='navbar-logo' src='https://i.imgur.com/UKU5YjC.jpg' />
+              <Nav.Link className='nav-text' href="#home">Home</Nav.Link>
+              <Nav.Link className='nav-text' href="#services">Services</Nav.Link>
+              <Nav.Link className='nav-text' href="#contact">Contact</Nav.Link>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text className="phone-num">
+            call us<a href="#login">770-123-4567</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
     </>
   )
 }
 
 export default Navigation
+
+
+
+      // <Navbar className='navigation' bg="transparent" variant="dark" expand="lg" fixed="top">
+      //   <Navbar.Brand className='nav-text' href="#">My Website</Navbar.Brand>
+      //   <Navbar.Toggle aria-controls="navbar-nav" />
+      //   <Navbar.Collapse id="navbar-nav">
+      //     <Nav className="ml-auto">
+      //     {/* <div className='nav-link-container'> */}
+      //       {/* <div className='nav-links-child'> */}
+      //         <Nav.Link className='nav-text' href="#home">Home</Nav.Link>
+      //         <Nav.Link className='nav-text' href="#services">Services</Nav.Link>
+      //         <Nav.Link className='nav-text' href="#contact">Contact</Nav.Link>
+      //       {/* </div> */}
+      //       <Navbar.Collapse className='phone-num'>
+      //         <Nav.Link className='nav-text justify-content-end' href="#contact">770-123-4567</Nav.Link>
+      //       </Navbar.Collapse>
+      //     {/* </div> */}
+
+      //     </Nav>
+      //   </Navbar.Collapse>
+      // </Navbar>
+
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,6 +3,7 @@ import { Container} from 'react-bootstrap'
 import Nav from '../components/Navigation'
 import Carousel from '../components/Carousel'
 import Services from '../components/Services'
+import Contact from '../components/Contact'
 
 
 const Home = () => {
@@ -11,21 +12,21 @@ return (
   <Container fluid className='cont'>
     <Nav/>
   <section className='section-1' >
-    <h1>Builder King Construction Group</h1>
-    <h2>Building your dreams</h2>
+    <h1 className='home-title-nav' >Builder King</h1>
+    <h1>Building your dreams</h1>
     <p className='home-about'>
       At Builder King we specializes in building custom homes and custom features for existing homes. We work with clients to understand their unique needs and preferences, and then design and builds homes and features to meet those specifications. Builder King also offers a range of customization options, including architectural design, interior design, and landscape design. Our goal is to create homes that are both functional and beautiful, and that reflect the individual style and personality of each client.
     </p>
     <Carousel/>
   </section>
 
-  <section>
-    <h1 className='section-2' >Services</h1>
+  <section className='section-2' >
+    <h1 className='service-title-nav' >Services</h1>
     <Services/>
   </section>
 
   <section>
-    <h1>bye</h1>
+    <Contact/>
   </section>
 </Container>
   )
