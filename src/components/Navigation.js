222aff9a841e470a8f861f7512335154b3fd4816
Taylor King <taylorking@taylors-mbp.lan>
2023-04-17 13:37:27 -0400

    media query

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,11 @@
 import React, {useState, useEffect} from 'react'
-import {Container, Nav, Navbar,Offcanvas, Image, Button} from 'react-bootstrap'
+import {Container, Nav, Navbar,NavDropdown, Image, Button} from 'react-bootstrap'
 
 
 const Navigation = () => {
 
   const [currentSection, setCurrentSection] = useState('home')
 
-  
 
   useEffect(() => {
     const observer = new IntersectionObserver(entries => {
@@ -30,26 +29,28 @@ const Navigation = () => {
 
   return (
     <>
-    <Navbar className='navigation' >
-      <Container fluid className='navigation-container' >
-        <Image className='navbar-logo' src='https://i.imgur.com/UKU5YjC.jpg' />
-              <Nav.Link className={currentSection === 'home' ? 'nav-active' : 'nav-text'} href="#home">Home</Nav.Link>
-              <Nav.Link className={currentSection === 'services' ? 'nav-active' : 'nav-text'} href="#services">Services</Nav.Link>
-              <Nav.Link className={currentSection === 'contact' ? 'nav-active' : 'nav-text'} href="#contact">Contact</Nav.Link>
-        <Navbar.Toggle />
-        <h1 className='nav-title' >
-          {currentSection === 'home' ? 'Builder King' : null}
-          {currentSection === 'services' ? 'Services' : null}
-          {currentSection === 'contact' ? 'Contact Us' : null}
-        </h1>
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text className="phone-num">
-            Call Us:<a href="tel:7708260035">770-826-0035</a>
-          </Navbar.Text>
+    <Navbar className='navigation' bg="light" expand="lg">
+      <Container fluid className='navigation-container'> 
+      <Image className='navbar-logo' src='https://i.imgur.com/UKU5YjC.jpg' />
+            <div className='nav-title' >
+              {currentSection === 'home' ? 'Builder King' : null}
+              {currentSection === 'services' ? 'Services' : null}
+              {currentSection === 'contact' ? 'Contact Us' : null}
+              <a className='phone-num' href="tel:7708260035">770-826-0035</a>
+            </div>
+
+            <div  className='hamburger'>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            </div>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+              <Nav.Link  href="#home">Home</Nav.Link>
+              <Nav.Link href="#services">Services</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
     </>
   )
 }
@@ -59,3 +60,24 @@ export default Navigation
 
 
 
+{/* <Navbar className='navigation' >
+      <Container fluid className='navigation-container' >
+        <Image className='navbar-logo' src='https://i.imgur.com/UKU5YjC.jpg' />
+              <Nav.Link className={currentSection === 'home' ? 'nav-active' : 'nav-text'} href="#home">Home</Nav.Link>
+              <Nav.Link className={currentSection === 'services' ? 'nav-active' : 'nav-text'} href="#services">Services</Nav.Link>
+              <Nav.Link className={currentSection === 'contact' ? 'nav-active' : 'nav-text'} href="#contact">Contact</Nav.Link>
+        <Navbar.Toggle />
+        
+        <div className='nav-title' >
+          {currentSection === 'home' ? 'Builder King' : null}
+          {currentSection === 'services' ? 'Services' : null}
+          {currentSection === 'contact' ? 'Contact Us' : null}
+          <a className='phone-num-small' href="tel:7708260035">770-826-0035</a>
+        </div>
+        <Navbar.Collapse className="justify-content-end"> 
+          <Navbar.Text className="phone-num">
+            Call Us:<a href="tel:7708260035">770-826-0035</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar> */}
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Col, Row, Container} from 'react-bootstrap'
 
 const Services = () => {
+
+    const [service, setService] = useState('construction')
+
+
   return (
-    <Container fluid className='service-container' >
+    <div className='service-container' >
         
-        <div className='service-1' >
+        <div className={service === 'construction' ? "service-1 show" : "service-1" } >
         <h1 className='service-title'>New Construction</h1>
         <div className='service-info-container' >
 
@@ -15,21 +19,31 @@ const Services = () => {
 
             
          </div>
+         
+         <div  className='service-buttons'>
+         <button class="button-30" role="button" onClick={() => {setTimeout(() => {setService('remodeling');}, 550);}}>Remodeling</button>
+            <button class="button-30" role="button" onClick={() => {setTimeout(() => {setService('management');}, 550);}} >Property Management</button>
+         </div>
+         
 
         </div>
 
-        <div className="service-2" >
+        <div className={service === 'remodeling' ? "service-2 show" : "service-2" } >
         <h1 className='service-title' >Remodeling</h1>
 
            <div className='service-info-container' >
             <img className='service-img' src='https://blacklinerenovations.com/wp-content/uploads/2020/04/Blog-Living-in-Your-House-During-a-Remodel-10-Proven-Tips-Featured.jpg'/>
 
-            <h5 className='service-info' >Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
+            <h5 className='service-info' >Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.    Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
 
+            </div>
+            <div  className='service-buttons'>
+                <button class="button-30" role="button" onClick={() => {setTimeout(() => {setService('management');}, 550);}}>Property Management</button>
+                <button class="button-30" role="button" onClick={() => {setTimeout(() => {setService('construction');}, 550);}}>New Construction</button>
             </div>
         </div>
         
-        <div className='service-3' >
+        <div className={service === 'management' ? "service-3 show" : "service-3" } >
         <h1 className='service-title' >Property Management</h1>
         <div className='service-info-container' >
             <img className='service-img' src='https://com.ohio.gov/wps/wcm/connect/gov/0c115c02-9baa-4cea-a5d4-300d4c086156/GettyImages-1207038160.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_K9I401S01H7F40QBNJU3SO1F56-0c115c02-9baa-4cea-a5d4-300d4c086156-nT57rqt'/>
@@ -37,9 +51,18 @@ const Services = () => {
             <h5 className='service-info' >Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
         </div>
 
-        </div>
+            <div className='service-buttons'>
+                <button class="button-30" role="button" onClick={() => {setTimeout(() => {setService('remodeling');}, 550);}}>Remodeling</button>
+                
+                <button class="button-30" role="button" onClick={() => {setTimeout(() => {setService('construction');}, 550);}}>New Construction</button>
+            </div>
 
-    </Container>
+            
+
+        </div>
+        
+
+    </div>
   )
 }
 
@@ -58,3 +81,10 @@ export default Services
         <h1>Property Management</h1>
     </Col>
 </Row> */}
+
+
+
+
+
+
+
