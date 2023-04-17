a1b06ec42fc62244f3986538eeb001db5bb1fb77
Taylor King <taylorking@taylors-mbp.lan>
2023-04-17 09:58:50 -0400

    adjustments to nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,50 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Container, Nav, Navbar,Offcanvas, Image, Button} from 'react-bootstrap'
 
 
 const Navigation = () => {
 
+  const [currentSection, setCurrentSection] = useState('home')
+
+  
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setCurrentSection(entry.target.id);
+        }
+      });
+    }, { threshold: 0.5 }); // The threshold determines how much of the section needs to be in view to trigger the intersection
+  
+    // Observe each section
+    document.querySelectorAll('section').forEach(section => {
+      observer.observe(section);
+    });
+  
+    // Cleanup
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <>
     <Navbar className='navigation' >
-      <Container fluid>
+      <Container fluid className='navigation-container' >
         <Image className='navbar-logo' src='https://i.imgur.com/UKU5YjC.jpg' />
-              <Nav.Link className='nav-text' href="#home">Home</Nav.Link>
-              <Nav.Link className='nav-text' href="#services">Services</Nav.Link>
-              <Nav.Link className='nav-text' href="#contact">Contact</Nav.Link>
+              <Nav.Link className={currentSection === 'home' ? 'nav-active' : 'nav-text'} href="#home">Home</Nav.Link>
+              <Nav.Link className={currentSection === 'services' ? 'nav-active' : 'nav-text'} href="#services">Services</Nav.Link>
+              <Nav.Link className={currentSection === 'contact' ? 'nav-active' : 'nav-text'} href="#contact">Contact</Nav.Link>
         <Navbar.Toggle />
+        <h1 className='nav-title' >
+          {currentSection === 'home' ? 'Builder King' : null}
+          {currentSection === 'services' ? 'Services' : null}
+          {currentSection === 'contact' ? 'Contact Us' : null}
+        </h1>
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text className="phone-num">
-            call us<a href="#login">770-123-4567</a>
+            Call Us:<a href="tel:7708260035">770-826-0035</a>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
@@ -29,23 +58,4 @@ export default Navigation
 
 
 
-      // <Navbar className='navigation' bg="transparent" variant="dark" expand="lg" fixed="top">
-      //   <Navbar.Brand className='nav-text' href="#">My Website</Navbar.Brand>
-      //   <Navbar.Toggle aria-controls="navbar-nav" />
-      //   <Navbar.Collapse id="navbar-nav">
-      //     <Nav className="ml-auto">
-      //     {/* <div className='nav-link-container'> */}
-      //       {/* <div className='nav-links-child'> */}
-      //         <Nav.Link className='nav-text' href="#home">Home</Nav.Link>
-      //         <Nav.Link className='nav-text' href="#services">Services</Nav.Link>
-      //         <Nav.Link className='nav-text' href="#contact">Contact</Nav.Link>
-      //       {/* </div> */}
-      //       <Navbar.Collapse className='phone-num'>
-      //         <Nav.Link className='nav-text justify-content-end' href="#contact">770-123-4567</Nav.Link>
-      //       </Navbar.Collapse>
-      //     {/* </div> */}
-
-      //     </Nav>
-      //   </Navbar.Collapse>
-      // </Navbar>
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,6 +4,7 @@ import Nav from '../components/Navigation'
 import Carousel from '../components/Carousel'
 import Services from '../components/Services'
 import Contact from '../components/Contact'
+import Intro from '../components/Intro'
 
 
 const Home = () => {
@@ -11,22 +12,20 @@ const Home = () => {
 return (
   <Container fluid className='cont'>
     <Nav/>
-  <section className='section-1' >
-    <h1 className='home-title-nav' >Builder King</h1>
-    <h1>Building your dreams</h1>
-    <p className='home-about'>
-      At Builder King we specializes in building custom homes and custom features for existing homes. We work with clients to understand their unique needs and preferences, and then design and builds homes and features to meet those specifications. Builder King also offers a range of customization options, including architectural design, interior design, and landscape design. Our goal is to create homes that are both functional and beautiful, and that reflect the individual style and personality of each client.
-    </p>
-    <Carousel/>
+  <section id='home' className='section-1' >
+    {/* <h1 className='home-title-nav' >Builder King</h1> */}
+    <Carousel className="component"/>
+    <Intro className="component" />
   </section>
 
-  <section className='section-2' >
-    <h1 className='service-title-nav' >Services</h1>
-    <Services/>
+  <section id='services' className='section-2' >
+    {/* <h1 className='service-title-nav' >Services</h1> */}
+    <Services className="component" />
   </section>
 
-  <section>
-    <Contact/>
+  <section id='contact' >
+    {/* <h1 id='contact' className='contact-title-nav' >Contact Us</h1> */}
+    <Contact className="component" />
   </section>
 </Container>
   )
